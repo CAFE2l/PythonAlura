c5f3704e371b8061af64a042f12621d8d68f1e11
Your Name <you@example.com>
2026-04-24 17:53:49 -0300

    unpacking tuples and lists

---
+++ Aulas/Aula06/idades.py
@@ -0,0 +1,20 @@
+idades = [15, 19, 8, 22, 14, 10, 19, 25]
+
+
+# for i in range(len(idades)):
+#     print(i, idades[i])
+
+
+usuarios = [
+    ("guilherme", 15, 2010),
+    ("daniela", 19, 2009),
+    ("paulo", 8, 2011),
+    ("maria", 22, 2008),
+    ("joão", 14, 2012),
+    ("ana", 10, 2013),
+    ("carlos", 19, 2010),
+    ("sofia", 25, 2005),
+]
+
+for nome, _, _ in usuarios:
+    print(nome)
