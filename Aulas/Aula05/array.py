be04933ee5599361641470e4652a5ad46912d0ee
Your Name <you@example.com>
2026-04-24 15:50:28 -0300

    understanding abstract methods

---
+++ Aulas/Aula05/array.py
@@ -0,0 +1,6 @@
+# import array as arr
+
+import numpy as np
+
+numeros = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+print(numeros)
--- Aulas/Aula05/heranca.py
+++ Aulas/Aula05/heranca.py
@@ -1,3 +1,6 @@
+from abc import ABCMeta, abstractmethod
+
+
 class Conta:
     def __init__(self, codigo):
         self.codigo = codigo
@@ -6,36 +9,9 @@ class Conta:
     def deposita(self, valor):
         self.saldo += valor
 
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
+
     def __str__(self):
         return f"Conta {self.codigo} Saldo: {self.saldo}"
-
-
-print(Conta(88))
-
-
-class ContaCorrente(Conta):
-    def passa_o_mes(self):
-        self.saldo -= 2
-
-
-class ContaPoupanca(Conta):
-    def passa_o_mes(self):
-        self.saldo *= 1.01
-        self.saldo -= 3
-
-
-conta_do_gui = ContaCorrente(15)
-conta_do_gui.deposita(1000)
-conta_do_gui.passa_o_mes()
-print(conta_do_gui)
-
-
-conta_da_dani = ContaPoupanca(17)
-conta_da_dani.deposita(1000)
-conta_da_dani.passa_o_mes()
-print(conta_da_dani)
-contas = [conta_do_gui, conta_da_dani]
-
-for conta in contas:
-    conta.passa_o_mes()
-    print(conta)
