cb05d917216ce61e6c88ed1ad22b897bc2ac8fce
Your Name <you@example.com>
2026-04-25 08:44:40 -0300

    course finished

--- Aulas/Aula06/contaSalario.py
+++ Aulas/Aula06/contaSalario.py
@@ -1,6 +1,8 @@
+from functools import total_ordering
 from operator import attrgetter
 
 
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
         self.codigo = codigo
@@ -12,6 +14,11 @@ class ContaSalario:
 
         return self.codigo == other.codigo and self.saldo == other.saldo
 
+    def __lt__(self, other):
+        if self.saldo != other.saldo:
+            return self.saldo < other.saldo
+        return self.codigo < other.codigo
+
     def deposita(self, valor):
         self.saldo += valor
 
@@ -19,27 +26,27 @@ class ContaSalario:
         return f"Conta Salário - Código: {self.codigo}, Saldo: {self.saldo}"
 
 
-conta_do_joao = ContaSalario(123)
-conta_do_joao.deposita(1000)
-print(conta_do_joao)
+conta_do_joao = ContaSalario(1023)
+conta_do_joao.deposita(100)
+# print(conta_do_joao)
 
 conta_da_maria = ContaSalario(123)
 conta_da_maria.deposita(1000)
-print(conta_da_maria)
+# print(conta_da_maria)
 
 conta_do_paulo = ContaSalario(456)
-conta_do_paulo.deposita(1000)
-print(conta_do_paulo)
+conta_do_paulo.deposita(100)
+# print(conta_do_paulo)
 
 contas = [conta_do_joao, conta_da_maria, conta_do_paulo]
-
-for conta in contas:
-    print(conta)
 
 
 def extrai_saldo(conta):
     return conta.saldo
 
 
-for conta in sorted(contas, key=attrgetter("saldo")):
+for conta in sorted(contas, key=attrgetter("saldo", "codigo")):
     print(conta)
+
+
+print(conta_do_joao <= conta_do_joao)
